71b936a463dbcbc7b1511da56f7582b29366f5fb
ssnalee <97poolbbang@naver.com>
2024-08-28 13:42:20 +0900

    uid number->string 변경 0828 13:42

--- src/components/referral/ReferralSubscript.vue
+++ src/components/referral/ReferralSubscript.vue
@@ -7,11 +7,7 @@
           :src="exchange.logo"
           :alt="`${exchange.name} 로고`"
         />
-        <img
-          class="logo_l"
-          :src="exchange.text"
-          :alt="exchange.name"
-        />
+        <img class="logo_l" :src="exchange.text" :alt="exchange.name" />
       </div>
       <p>레퍼럴 페이백 신청</p>
       <button class="gradient_green">지금 가입하기</button>
@@ -22,10 +18,14 @@
       (다른 링크나 코드로 가입 시 페이백이 이루어지지 않습니다.)
     </p>
     <div class="uid_area flex_row_c_c">
-      <label for="userUid"
-        >{{ exchange.name }} 거래소 본인 UID :
-      </label>
-      <input type="number" id="userUid" name="userUid" v-model="userUid"/>
+      <label for="userUid">{{ exchange.name }} 거래소 본인 UID : </label>
+      <input
+        type="text"
+        id="userUid"
+        name="userUid"
+        v-model="userUid"
+        @keydown="checkNumber"
+      />
     </div>
     <div class="btn_area flex_row_c_c">
       <button class="gradient_blue" @click="enrollBtn">신청하기</button>
@@ -33,7 +33,7 @@
     <div>
       <ul class="flex_row_c_c">
         <li class="flex_row_c_c">
-          <p>{{ exchange.name}} 가입 방법</p>
+          <p>{{ exchange.name }} 가입 방법</p>
           <button>
             <img src="@/assets/image/download_icon.png" alt="download" />
           </button>
@@ -63,18 +63,22 @@ import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 const router = useRouter();
 const store = useStore();
-const exchange = computed(()=>{
-    return store.state.referral.exchange;
+const exchange = computed(() => {
+  return store.state.referral.exchange;
 });
 const modalState = computed(() => {
   return store.state.referral.modalState;
 });
-const uidState = computed(()=>{
-    return store.state.referral.uidState;
+const uidState = computed(() => {
+  return store.state.referral.uidState;
 });
 let msgCode = "msgCode0"; //입력하지않았을 때
 let userUid = "";
-
+const checkNumber = (e) => {
+  if (!/^\d$/.test(e.key)) {
+    e.preventDefault();
+  }
+};
 const enrollBtn = async () => {
   store.commit("referral/setUId", userUid);
   await store.dispatch("referral/postUid");
@@ -83,17 +87,15 @@ const enrollBtn = async () => {
   //msg code 1 = 승인대기, 2 = 승인완료, 3 =승인실패, 4 = 이미 등록
 };
 
-if(Object.keys(exchange.value).length === 0) {
+if (Object.keys(exchange.value).length === 0) {
   router.push("/payback");
 }
-const createdFn = async () =>{
+const createdFn = async () => {
   await store.dispatch("referral/postCheckLogin");
-}
-store.watch((state)=>{
-  if(state.referral.setting){
+};
+store.watch((state) => {
+  if (state.referral.setting) {
     createdFn();
-  } 
-},createdFn);
-
-
+  }
+}, createdFn);
 </script>
