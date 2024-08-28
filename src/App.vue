adef213503d17d386a99db281e78717d0af2bec5
ssnalee <97poolbbang@naver.com>
2024-08-28 21:07:37 +0900

    modal 추가 0828 21:07

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
     <TheSideMenu></TheSideMenu>
     <div class="main_wrap">
       <router-view></router-view>
-      <!-- <ModalMsg msg="noLogin" v-if="!loginStatus"/> -->
+      <ModalMsg msg="noLogin" v-if="!loginStatus"/>
     </div>
   </div>
 </template>
@@ -19,7 +19,7 @@ import TheHeader from "./components/comn/TheHeader.vue";
 import TheSideMenu from "./components/comn/TheSideMenu.vue";
 import { useStore } from "vuex";
 import { computed } from "vue";
-// import ModalMsg from "./components/modal/ModalMsg.vue";
+import ModalMsg from "./components/modal/ModalMsg.vue";
 import { useRouter } from "vue-router";
 const { t, locale } = useI18n({ useScope: "global" });
 const store = useStore();
@@ -30,14 +30,27 @@ const session_id = computed(()=>{
 const retri_id = computed(()=>{
   return store.state.referral.retri_id;
 });
-
-// const loginStatus = computed(()=>{
-//   return store.state.referral.loginStatus === 200 ? true : false;
+const loginStatus = computed(()=>{
+  return store.state.referral.loginStatus === 200 ? true : false;
+});
+// router.beforeEach(function (to, from, next) {
+//   // to: 이동할 url에 해당하는 라우팅 객체
+//   if (to.matched.some(function(routeInfo) {
+//     return routeInfo.meta.authRequired;
+//   })) {
+//     // 이동할 페이지에 인증 정보가 필요하면 경고 창을 띄우고 페이지 전환은 하지 않음
+//     alert('Login Please!');
+//   } else {
+//     console.log("routing success : '" + to.path + "'");
+//     next(); // 페이지 전환
+//   };
 // });
 const createdFetch = async () =>{
   // await store.dispatch("referral/postRetriAuth");
   await store.dispatch("referral/getLoadUser");
   store.commit("referral/initSetting",true);
+  console.log(session_id.value);
+  console.log(retri_id.value);
   if(!session_id.value || !retri_id.value){
     router.push('/404');
   }
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,4 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
 export default router;
